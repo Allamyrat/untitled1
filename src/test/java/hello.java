211cf4b478a72cabc993e195f2e392792ec2cd03
Allamyrat <allamyrat.bayramov@gmail.com>
2020-10-17 12:29:49 +0700

    change in string

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("warm hello");
+        System.out.println("local hello");
         //
         //
         // more
